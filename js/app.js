2c226ccc21a3b7700af973fedb67b4cbe6b53bbf
jacktulv <shkinglu@163.com>
2017-12-03 17:50:26 +0800

    finish basic function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,24 @@
  */
 
 
+//画HTML
+$(drawGird);
+ function drawGird(){
+   $('.moves').html(count);
+   //定义卡片图标类型
+   var CardsType=['fa fa-diamond','fa fa-paper-plane-o','fa fa-anchor','fa fa-bolt',
+   'fa fa-cube','fa fa-leaf','fa fa-diamond','fa fa-paper-plane-o','fa fa-anchor',
+   'fa fa-bolt','fa fa-cube','fa fa-leaf'];
+  //定义卡牌
+   var Card=$('<li class="card"><i></i></li>');
+   shuffle(CardsType);
+   for(var i=0;i<12;i++){
+      Card.children('i').addClass(CardsType[i]);
+      $('.deck').append(Card.clone());
+      Card.children('i').removeClass(CardsType[i]);
+   }
+ }
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -24,8 +42,6 @@ function shuffle(array) {
 
     return array;
 }
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +52,54 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ var Cards=new Array();
+ var count=0,matched=0;//记数和匹配
+
+$(function(){
+  //处理重置
+  $('div.restart').on('click', function(){
+     count=0;matched=0;
+     Cards=[];
+     $('.deck').html('');
+     drawGird();
+  });
+
+  //处理点击事件
+  $('.deck').on('click','li',function(){
+    count++;
+    $('.moves').html(count);
+    var Card = $(this);
+    var CardsType=Card.children().attr("class");
+    //保证是未翻的牌
+    if(Card.hasClass("open")||Card.hasClass("match")||Card.hasClass("show")){
+        alert("此牌已翻");
+    }
+
+
+    else if(Cards.length===0){
+      Card.addClass("open show");
+      Cards.push(CardsType);
+    }else{
+          if(Cards.includes(CardsType)){
+              Card.addClass("show match");
+              $('li.open').toggleClass("open match");
+              Cards.pop();
+              matched++;
+              if(matched===6){
+                setTimeout(function(){
+                  alert("恭喜您获得胜利 \n 共用"+count+"步 重新开始请点刷新")},500);
+              }
+          }else{
+              Card.addClass("open show");
+              setTimeout(function(){
+              Card.removeClass("open show");
+              $('li.open').toggleClass("open show");
+              Cards.pop();
+            },200);
+        }
+      }
+
+
+  });
+})
